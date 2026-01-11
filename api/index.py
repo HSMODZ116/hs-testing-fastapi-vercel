19baffbd4f241956bf8ad0504cc9458885ead58a
Haseeb Sahil <hsmodzofc@gmail.com>
2026-01-12 04:34:31 +0500

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -1,19 +1,31 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+import os, sys
 
-from plugins.tmail import router as tmail_router
+# Ensure imports work on Vercel
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
 
-app = FastAPI(title="A360 Temp Mail API")
+from plugins.fb import router as fb_router
+from plugins.tik import router as tik_router
+from plugins.pnt import router as pnt_router
 
-app.include_router(tmail_router)
+app = FastAPI(title="A360 Social Downloader API (Fb/Tik/Pnt)")
+
+app.include_router(fb_router, tags=["Facebook Downloader"])
+app.include_router(tik_router, tags=["TikTok Downloader"])
+app.include_router(pnt_router, tags=["Pinterest Downloader"])
 
 @app.get("/")
 async def root():
-    return JSONResponse(content={
+    return {
         "status": "ok",
         "endpoints": [
-            "/tmail/gen",
-            "/tmail/cmail"
-        ],
-        "docs": "/docs"
-    })
+
+            "/fb/dl?url=",
+            "/tik/dl?url=",
+            "/pnt/dl?url=",
+            "/docs"
+        ]
+    }
