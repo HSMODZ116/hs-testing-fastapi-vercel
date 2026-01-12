38258660015d167b70aacbbaffed1b864f7229ec
Haseeb Sahil <hsmodzofc@gmail.com>
2026-01-12 05:33:57 +0500

    Delete utils directory

--- utils/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-LOGGER = logging.getLogger("a360")
-if not LOGGER.handlers:
-    handler = logging.StreamHandler()
-    fmt = logging.Formatter("[%(levelname)s] %(message)s")
-    handler.setFormatter(fmt)
-    LOGGER.addHandler(handler)
-LOGGER.setLevel(logging.INFO)
